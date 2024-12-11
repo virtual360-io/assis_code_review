a7a80ea16f6b0449b9d78761f63bfe63574fd259
Victor Campos <victorcampos@ufrj.br>
2024-12-11 09:39:49 -0300

    feat: make assis prompt be passed as argument

--- index.js
+++ index.js
@@ -30,9 +30,7 @@ async function run() {
       })
       .join("diff --git "); // Rejoin with the diff header
 
-    const question =
-      " You a Ruby Full Stack Developer CTO. You should review this change and give a 0 to 10 grate to it. If you give a 0, it means that this code is not good, not performact, insecure, and with bad variable names. 1 to 9 it means that code is not ready for production, 9 is better than 1. 10 means that this code is ready to go to production, with good variable names, good performance, and secure, and have automate tests, dont need do be perfect. Dont use Documentation to give your grate. Please, give tip to improve to 10. If you think the code needs more automated tests, please, suggest the tests that you think are necessary. You use Minitest to test your Ruby code.\n\n" +
-      filteredDiff;
+    const question = core.getInput("assis-prompt") + "\n\n" + filteredDiff;
 
     // Send to API
     const response = await fetch(apiUrl, {
